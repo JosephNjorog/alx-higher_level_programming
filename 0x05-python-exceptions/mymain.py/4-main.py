a0dc1e7937ef309ddab039670a632b5f24ec53f1
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-18 09:35:44 +0300

    Update 4-main.py

--- 0x05-python-exceptions/mymain.py/4-main.py
+++ 0x05-python-exceptions/mymain.py/4-main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-raise_exception = __import__('5-raise_exception').raise_exception
+list_division = __import__('4-list_division').list_division
 
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
