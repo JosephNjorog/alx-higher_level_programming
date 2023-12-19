016f1b0a736a5ec642ade91ab9e501d92e1c431b
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-20 01:51:13 +0300

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module containing square class"""
+
+
+class Square:
+    """representing a square"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """return the square area"""
+
+        return (self.__size)**2
