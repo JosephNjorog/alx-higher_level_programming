6f23e4aa0b7dbb4d78807b897043777d06008ac9
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-20 01:49:56 +0300

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module containing square class"""
+
+
+class Square:
+    """representing a square"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
