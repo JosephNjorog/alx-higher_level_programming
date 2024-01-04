a6eb36635cc4af6d7e89d105e896149a02269ed7
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2024-01-05 00:13:53 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,7 +4,6 @@ nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
 
-
 from sys import argv
 
 if __name__ == "__main__":
@@ -12,7 +11,7 @@ if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if argv[1].isdigit() is False:
+    if not argv[1].isdigit():
         print("N must be a number")
         exit(1)
     n = int(argv[1])
@@ -33,10 +32,10 @@ if __name__ == "__main__":
 
     def reject(x, y):
         """determines whether or not to reject the solution"""
-        if (already_exists(y)):
+        if already_exists(y):
             return False
         i = 0
-        while(i < x):
+        while i < x:
             if abs(a[i][1] - y) == abs(i - x):
                 return False
             i += 1
@@ -47,16 +46,21 @@ if __name__ == "__main__":
         for i in range(x, n):
             a[i][1] = None
 
+    def print_solution():
+        """prints the solution in the required format"""
+        for i in range(n):
+            print([i, a[i][1]])
+
     def nqueens(x):
         """recursive backtracking function to find the solution"""
         for y in range(n):
             clear_a(x)
             if reject(x, y):
                 a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
+                if x == n - 1:  # accepts the solution
+                    print_solution()
                 else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+                    nqueens(x + 1)  # moves on to the next x value to continue
 
     # start the recursive process at x = 0
     nqueens(0)
